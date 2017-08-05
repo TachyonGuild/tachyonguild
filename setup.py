fba03c636ec44571e5720a4d9e025525037bb671
tachyonguild <>
2017-08-05 19:56:32 +0200

    Modularized.

---
+++ setup.py
@@ -0,0 +1,4 @@
+from setutools import setup
+
+setup(name='tachyonguild', version='0.1', description='tachyonguild', url='https://github.com/TachyonGuild/tachyonguild', author='tachyonguild', author_email='', license='MIT', packages=['tachyonguild'], zip_safe=False)
+
